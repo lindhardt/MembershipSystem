235a834f38a64549cdd82bf695c1e99b7467d593
Mikkel Hansen <mikkelhansen@outlook.dk>
2017-09-20 13:41:24 +0200

    Add navigation properties

--- MembershipSystem/Models/IdentityModels.cs
+++ MembershipSystem/Models/IdentityModels.cs
@@ -1,9 +1,14 @@
 ﻿using Microsoft.AspNet.Identity.EntityFramework;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace MembershipSystem.Models
 {
     public class UserRole : IdentityUserRole<int>
     {
+        [ForeignKey(nameof(UserId))]
+        public virtual User User { get; set; }
+        [ForeignKey(nameof(RoleId))]
+        public virtual Role Role { get; set; }
     }
 
     public class UserLogin : IdentityUserLogin<int>
