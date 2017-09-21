4775a5b248b8987188d25f8fe5b4e14275dab3c6
Jonas Bode <tripozide@gmail.com>
2017-09-21 12:21:30 +0200

    About page update
    About page update

--- MembershipSystem/Controllers/HomeController.cs
+++ MembershipSystem/Controllers/HomeController.cs
@@ -13,5 +13,10 @@ namespace MembershipSystem.Controllers
         {
             return View();
         }
+
+        public ActionResult About()
+        {
+            return View();
+        }
     }
 }
